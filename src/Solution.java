dc92493b4db080761f130be47fd06cc44a870fc0
umer <syedumerahmedcode@gmail.com>
2026-02-24 06:40:30 +0500

    Feature: Implemented the solution and tested it against a valid test case scenario.

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,41 @@
+
+
 public class Solution {
     public void setZeroes(int[][] matrix) {
-
+      Boolean firstColumn = false;
+        int row = matrix.length;
+        int column = matrix[0].length;
+        
+        for(int i=0 ; i<row; i++){
+            if(matrix[i][0] == 0){
+                firstColumn = true;
+            }
+            for(int j=1; j<column; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for(int i = 1; i<row; i++){
+            for(int j=1; j<column; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if(matrix[0][0] == 0){
+            for (int j=0; j<column; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if(firstColumn){
+            for (int i=0; i<row; i++){
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
